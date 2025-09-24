5eddc392993bc4278521423b3438e09fa866aff1
Valerieee01 <zharmel.afa@gmail.com>
2025-09-23 23:08:01 -0500

    Inicio BK

---
+++ app.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv"; // trabajar con el archivo de configuracion 
+import cors from "cors";// para trabajar fuera del dominio
+import bodyParser from "body-parser"; //parsear de lo que recibire en la informacion
+import cookieParser from "cookie-parser"; // autenticacion para enviar una cookie
+
+dotenv.config();
+
+// Crear la instancia de Express
+const app = express();
+// Middleware
+// Habilita CORS
+app.use(cors()); 
+// Permite que la app acepte datos JSON
+app.use(bodyParser.json()); 
+// app.use(express.json());
+// Permite el envio de datos de tipo utlencode
+app.use(express.urlencoded({ extended: true }));
+// Permite manejar cookies en las respuestas.
+app.use(cookieParser());
+
+// Rutas
+
+
+// Puerto para ejecutar el servidor
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
